e5c24461dfaab681f6d0c341a69f9373d4b22a5d
Tom Chege <emdeechege@gmail.com>
2018-09-26 14:13:12 +0300

    adding navigation urls

--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to Gallery")
+    return render(request, 'welcome.html')
