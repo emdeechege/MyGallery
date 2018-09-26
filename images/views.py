ca385255aa829a7eb48f698a219c0d89bfa54b0d
Tom Chege <emdeechege@gmail.com>
2018-09-26 16:28:02 +0300

    foreign key references the class above it

--- images/views.py
+++ images/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-import datetime as dt
 
 # Create your views here.
-def welcome(request):
+def home(request):
     return render(request, 'all-images.html')
 
 def image(request,image_id):
