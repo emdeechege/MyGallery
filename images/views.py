7af31c3c7933662153c8fa82722281e1af13602d
Tom Chege <emdeechege@gmail.com>
2018-09-26 15:27:23 +0300

    preparing database configurations

--- images/views.py
+++ images/views.py
@@ -5,3 +5,10 @@ import datetime as dt
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-images/image.html", {"image":image})
--- pictures/settings.py
+++ pictures/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'images',
     'bootstrap4',
-    'images.apps.ImagesConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,8 +77,10 @@ WSGI_APPLICATION = 'pictures.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'emdee',
+    'PASSWORD':'arif@123',
     }
 }
 
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
