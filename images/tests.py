10c8334dc6c8f51f751a8549103c9a0480ab6dae
Tom Chege <emdeechege@gmail.com>
2018-09-28 19:47:42 +0300

    all tests running ok~

--- images/tests.py
+++ images/tests.py
@@ -35,3 +35,34 @@ class ImageTest(TestCase):
         self.drinks.save()
         images = Image.all_images()
         self.assertTrue(len(images) > 0)
+
+    def test_search_by_category(self):
+        self.drinks.save()
+        images = Image.search_by_category('fun')
+        self.assertTrue(len(images) > 0)
+
+    def test_view_location(self):
+        self.drinks.save()
+        location = Image.view_location(self.nairobi)
+        self.assertTrue(len(location) > 0)
+
+    def test_view_category(self):
+        self.drinks.save()
+        categories = Image.view_category(self.music)
+        self.assertTrue(len(categories) > 0)
+
+class categoriesTest(TestCase):
+    def setUp(self):
+        self.nature = categories(name='nature')
+
+    def test_instance(self):
+        self.nature.save()
+        self.assertTrue(isinstance(self.nature, categories))
+
+class LocationTest(TestCase):
+    def setUp(self):
+        self.nairobi = Location(name='nairobi')
+
+    def test_instance(self):
+        self.nairobi.save()
+        self.assertTrue(isinstance(self.nairobi, Location))
